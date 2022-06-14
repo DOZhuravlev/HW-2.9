15d7cd931bcd4c297e9877db9134e1e10bce3ed4
Zhuravlev Dmitry <qwerty-91@list.ru>
2022-06-14 17:32:14 +0500

    Initial Commit

---
+++ HW 2.9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2.9
+//
+//  Created by Zdrenko Zigich on 14.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
